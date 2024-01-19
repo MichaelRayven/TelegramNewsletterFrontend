e34dcce628aac5c38120eaac493dd5660c0df9c4
MichaelRayven <themikerayven@gmail.com>
2024-01-19 07:39:17 +0700

    Added checkbox component

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { MdCheckBox, MdOutlineCheckBox } from "react-icons/md"
+import "./Checkbox.scss"
+
+interface Props {
+    value?: boolean;
+    children?: React.ReactNode;
+}
+
+const Checkbox = ({ children, value }: Props) => {
+    const [checked, setChecked] = useState(value || false)
+    
+    const getCheckboxIcon = () => checked ? 
+        <MdCheckBox /> :
+        <MdOutlineCheckBox />
+
+    return (
+        <div className="checkbox">
+            <button 
+                id="checkbox"
+                name="checkbox"
+                className="button__icon" 
+                onClick={() => {setChecked(!checked)}}>
+                {getCheckboxIcon()}
+            </button>
+            <label htmlFor="checkbox" className="link checkbox__content">
+                {children}
+            </label>
+        </div>
+    )
+}
+
+export default Checkbox
