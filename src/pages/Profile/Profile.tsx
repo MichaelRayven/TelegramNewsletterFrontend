f959f5102b52d13062192af9dc4ff193e4c4d060
EnterNoName <mihailzinakovvv@gmail.com>
2024-02-12 21:14:13 +0700

    Added profile component

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import "./Profile.scss"
+
+interface Props {
+
+}
+
+const ProfilePage = ({}: Props) => {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default ProfilePage;
