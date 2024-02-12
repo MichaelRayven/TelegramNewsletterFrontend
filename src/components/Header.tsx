bb00e33224ee39f577fd168fdd13519a7ee4abb3
EnterNoName <mihailzinakovvv@gmail.com>
2024-02-12 21:13:18 +0700

    Added dropdown to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import "./Header.scss"
 import Button from "./Button"
 import { useNavigate } from "react-router-dom"
 import Logo from "./Logo";
+import { useAppSelector } from "@/config/redux/hooks";
+import { selectUser } from "@/config/redux/slices/accountSlice";
+import Dropdown, { DropdownItem } from "./Dropdown";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const user = useAppSelector(selectUser)
+  
   return (
     <header className="header container">
         <div className="header__inner">
@@ -19,7 +23,15 @@ const Header = () => {
           </nav>
           <div className="header__actions">
             <a className="link" href="">What's new?</a>
-            <Button onClick={() => navigate("/signin")}>Sign in</Button>
+            {user ? 
+              <Dropdown direction={"top"} content={<p>Test</p>}>
+                <DropdownItem>Account</DropdownItem>
+                <DropdownItem>My Bots</DropdownItem>
+                <DropdownItem>Subscription Plan</DropdownItem>
+                <DropdownItem>Sign Out</DropdownItem>
+              </Dropdown> :
+              <Button onClick={() => navigate("/signin")}>Sign in</Button>
+            }
           </div>
         </div>
     </header>
